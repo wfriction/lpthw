9370438299951e2108249d206e3be231a813ec50
wfriction <chris+github@withoutfriction.com>
2010-12-16 15:44:15 +1100

    finished ex36

--- ex36.py
+++ ex36.py
@@ -6,33 +6,23 @@ import platform
 
 os = platform.system()
 
-def call_os(os, q):
-    if os == 'Linux':
-        Linux(q)
-    elif os == 'Darwin':
-        Darwin(q)
-    elif os == 'Windows':
-        Windows(q)
-
-def Linux(q):
+def qnfinder(q):
     if q == 0:
-        paper(os)
+        paper()
     elif q == 1:
-        ios(os)
+        ios()
 
-# def Windows():
-# def Darwin():
-def paper(os):
+def paper():
     paper = raw_input("Do you have no desire for backups or searchability? (y/n)")
     if paper == 'y':
         solution('Pen and paper')
     elif paper == 'n':
-        call_os(os, 1)
+        qnfinder(1)
     else:
         print "Please type y or n, without the quotes."
         paper()
-def ios(os):
-    ios = raw_input("Do you live in your iPhone and iPad? (y/n")
+def ios():
+    ios = raw_input("Do you live in your iPhone and iPad? (y/n)")
     if ios == 'y':
         research('ios')
     else:
@@ -69,10 +59,9 @@ def check_platform():
     platforms = ['Windows', 'Darwin', 'Linux']
     for os in platforms:
         if platform.system() == os:
-            os = raw_input("Is %s your most used os? If not, enter what is." % os)
+            os = raw_input("Is %s your most used os? If not, enter what is. (Linux/Windows/Darwin)" % os)
             os = platform.system()
-            Linux(0)
-#            os(0)
+            qnfinder(0)
 
 def start():
     script, player = argv
